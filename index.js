fac268b49308f7797a62f9cb807df21bacf5b9e0
Naznin <nazninmalek111@gmail.com>
2018-05-07 18:07:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function
 whisper(string){
   return string.toLowerCase()
 }
+
+function
+logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function
+logShout(string){
+  console.log(string.toUpperCase())
+}
